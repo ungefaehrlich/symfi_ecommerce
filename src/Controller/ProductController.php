2c5c2b5117e1c6b5e75645f00664ec80ba8e540d
ungefaehrlich <marcstipcevic@gmail.com>
2020-12-20 19:44:21 +0100

    add easyadmin and start products index

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('product/index.html.twig', [
-
+            'products' => $productRepository->findAll()
         ]);
     }
 }
